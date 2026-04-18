19a0243121441971497425b7b52644d47c598a2a
Dark Code <dark@Darks-MacBook-Air.local>
2026-04-18 15:49:02 +0530

    Basic Api

--- src/app.js
+++ src/app.js
@@ -14,10 +14,12 @@ const authRouter = require("./routes/auth.routes");
 const accountRouter = require("./routes/account.routes");
 const transactionRouter = require("./routes/transaction.routes");
 
-
 /**
  * -Use Routes
  */
+app.get("/", (req, res) => {
+  res.send("Ledger Service is up and Running");
+});
 app.use("/api/auth", authRouter); //Auth Router
 app.use("/api/accounts", accountRouter); //Account Router
 app.use("/api/transactions", transactionRouter); //Transaction Router
